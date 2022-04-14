526077b51043c716729aceec7e07e1becfff1e77
christianvionnet <christvionnet@gmail.com>
2022-04-14 08:48:47 -0300

    Add heroku< needed config

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const logger = require("./loggerMiddleware");
 
 app.use(cors());
 app.use(express.json());
+
+app.use(logger);
 
 let notes = [
   {
@@ -67,7 +70,8 @@ app.post("/api/notes", (request, response) => {
   response.status(201).json(newNote);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
